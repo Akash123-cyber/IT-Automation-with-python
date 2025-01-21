0e8c5734ffbc2062bfbe896531f92d9047f91786
Akash Kumar Malik <75205586+Akash123-cyber@users.noreply.github.com>
2025-01-21 15:49:28 +0530

    Add files via upload
    Code For Checking Armstrong Number  In Python

---
+++ Armstrong_number.py
@@ -0,0 +1,47 @@
+##armstrong number
+##Armstrong number whose sum of digits raise to the {power==(no. of digits in the number)} equals to 
+### the number itself 
+# x=int(input("enter the number: "))
+# l=len(str(x))
+# k=0
+# n=x
+# for i in range(l):
+#     r=x%10
+#     k=k+r**l
+#     x=x//10
+# if k==n:
+#     print("{} is an armstrong number".format(n))
+# else:
+#     print("{} is not an armstrong number".format(n))
+
+############################################################################################
+### i want to create armstrong numbers upto n terms
+##WRONG
+##WRONG
+##WRONG
+##WRONG
+##WRONG
+# n=int(input("enter the number of terms: "))
+
+# count=0
+# x=0
+
+# while count!=n:
+#     k=0
+#     for i in str(x+1):
+#         k=k+int(i)**len(str(x+1))
+#         if k==x:
+#             print(x)
+#             count=count+1
+#     x=x+1
+
+
+
+
+
+
+
+
+
+
+
